36168d564f36f6b54ff30531a9c443ee463f8293
akashthorat01 <akashathorat2020@gmail.com>
2026-03-21 03:49:23 +0530

    fixed mobile spacing and bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
   ];
 
   return (
-    <section id="about" className="py-20 px-6 md:px-12 relative z-10">
+    <section id="about" className="py-10 px-6 md:px-12 relative z-10">
       <div className="max-w-7xl mx-auto">
         <motion.div 
           initial={{ opacity: 0, y: 20 }}
